ca410f034942f721d9bcbd6e5c22497320e83283
Nicholas Lammers <nlammers@berkeley.edu>
2024-02-13 10:56:13 -0800

    Changes to run on local machine

--- napari_visualization_cellpose.py
+++ napari_visualization_cellpose.py
@@ -6,22 +6,25 @@ import glob2 as glob
 import numpy as np
 # from czitools import misc_tools
 # set parameters
-image_folder = "E:\\Nick\\Cole Trapnell's Lab Dropbox\\Nick Lammers\\Nick\\killi_tracker\\built_data\\231016_EXP40_LCP1_UVB_300mJ_WT_Timelapse_Raw\\"
+# root = "E:\\Nick\\Cole Trapnell's Lab Dropbox\\Nick Lammers\\Nick\\killi_tracker\\built_data\\""
+root = "/Users/nick/Cole Trapnell's Lab Dropbox/Nick Lammers/Nick/killi_tracker/built_data/"
+project_name = "231016_EXP40_LCP1_UVB_300mJ_WT_Timelapse_Raw"
+image_folder = os.path.join(root, project_name, "")
 image_list = sorted(glob.glob(os.path.join(image_folder + "*.tiff")))
-label_folder = "E:\\Nick\\Cole Trapnell's Lab Dropbox\\Nick Lammers\\Nick\\killi_tracker\\built_data\\cellpose_output_test2\\231016_EXP40_LCP1_UVB_300mJ_WT_Timelapse_Raw\\"
+# label_folder = "E:\\Nick\\Cole Trapnell's Lab Dropbox\\Nick Lammers\\Nick\\killi_tracker\\built_data\\cellpose_output_test2\\231016_EXP40_LCP1_UVB_300mJ_WT_Timelapse_Raw\\"
 
-label_list = sorted(glob.glob(os.path.join(label_folder + "*_labels.tif")))
-prob_list = sorted(glob.glob(os.path.join(label_folder + "*_probs.tif")))
+# label_list = sorted(glob.glob(os.path.join(label_folder + "*_labels.tif")))
+# prob_list = sorted(glob.glob(os.path.join(label_folder + "*_probs.tif")))
 image_ind = 0
 
 im = io.imread(image_list[image_ind])
-lb = io.imread(label_list[image_ind])
-pr = io.imread(prob_list[image_ind])
+# lb = io.imread(label_list[image_ind])
+# pr = io.imread(prob_list[image_ind])
 # im = im / np.max(im)
 # im_rs = resize(im, (im.shape[0]*4, im.shape[1]*4), preserve_range=True).astype(np.uint16)
 viewer = napari.view_image(im)
-probs = viewer.add_image(pr, name="probabilities", contrast_limits=[-8, 8])
-labels = viewer.add_labels(lb, name="labels")
+# probs = viewer.add_image(pr, name="probabilities", contrast_limits=[-8, 8])
+# labels = viewer.add_labels(lb, name="labels")
 
 
 if __name__ == '__main__':
