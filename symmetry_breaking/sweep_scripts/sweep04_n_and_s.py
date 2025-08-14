5a74fe437436c9113e359be16892410fab9621d6
Nick <nlammers@berkeley.edu>
2025-08-13 23:52:33 -0700

    Prep for basic param check sweep

--- symmetry_breaking/sweep_scripts/sweep04_n_and_s.py
+++ symmetry_breaking/sweep_scripts/sweep04_n_and_s.py
@@ -6,7 +6,7 @@ from multiprocessing import Pool, Lock, Manager
 import os
 import time
 import uuid
-from symmetry_breaking.models.NLP_neutralization_1D import NodalLeftyNeutralization1D
+from symmetry_breaking.models.NL_field_1D import NodalLeftyField1D
 from symmetry_breaking.models.trackers import NodalROITracker
 from symmetry_breaking.models.sweep import make_param_dicts, ensure_output_dir, run_and_save_single_simulation
 
@@ -18,44 +18,42 @@ output_file = "sweep_results.csv"
 if __name__ == "__main__":
     import multiprocessing as mp
     from pathlib import Path
-    sweep_name = "sweep03_extinction"
+    sweep_name = "sweep04_n_and_s"
     root = Path("/media/nick/hdd021/Cole Trapnell's Lab Dropbox/Nick Lammers/Nick/symmetry_breaking/pde/sweeps/")
     # Settings
     # use_random = True  # or False for full grid
     n_workers = 28 #np.max([1, int(mp.cpu_count() / 2.1)])
-    grid_type = "grid" #"random" if use_random else "grid"
-    n_samples = 2500
+    grid_type = "lhs" #"random" if use_random else "grid"
+    n_samples = 1000
 
     # hyperparams
     dx = 10
-    L = 3000
+    L = 3500
     T = 10 * 60 * 60
     # dt = 0.5 * dx ** 2 /60 / 1.25 # Stability condition for diffusion
 
     param_grid = {
-        "N_amp": np.linspace(0.1, 12000, 50),
-        "L_value": np.linspace(0.1, 12000, 50),
+        "K_I": np.logspace(1, 1e6, 10),
+        "D_L": np.logspace(np.log10(1.85*2), np.log10(60), 10),
+        "sigma_N": np.logspace(-4, 2, 10),
     }
 
     static_params = {
+        "N_amp": 100,
         'K_NL': 138.9495494373,
         'K_A': 193.0697728883,
         # 'K_R': 19.3069772888,
-        'K_I': 19.3069772888,
+        # 'K_I': 19.3069772888,
         'mu_L': 0.0002275846,
         'N_sigma': 30,
         'sigma_N': 10.0,
         'sigma_L': 10.0,
-        'D0_N': 1.85,
-        'D0_L': 15.0,
-        'alpha_L': 0,
-        'alpha_N': 0,
-        'tau_rho': 3600,
+        'D_N': 2*1.85,
+        # 'D_L': 15.0,
         'n': 2,
         'm': 1,
         'p': 2,
         'q': 2,
-        "no_density_dependence": True,
         "L_init": "constant"
     }
 
@@ -63,16 +61,16 @@ if __name__ == "__main__":
         "dx": dx,
         "L": L,
         "T": T,
-        "model_class": NodalLeftyNeutralization1D,
+        "model_class": NodalLeftyField1D,
         "tracker_class": NodalROITracker,
         "interval": 300,
     }
 
     # --- compute CFL-safe dt once (using max D0) ---
     # dx = 10
-    Dmax = max(static_params["D0_N"], static_params["D0_L"])
+    # Dmax = max(static_params["D_N"], static_params["D_L"])
     # dt = min(sim_config.get("dt", 1e9), dt_cfl(dx, Dmax, safety=0.5))  # keep your old dt if smaller
-    sim_config["dt"] = 0.5 * dx ** 2 / Dmax / 1.25
+    sim_config["dt"] = 0.5 * dx ** 2 / 60 / 1.25
 
     param_dicts = make_param_dicts(param_grid, grid_type=grid_type, n_samples=n_samples, seed=42)
 
