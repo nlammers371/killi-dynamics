b6418f1b8af25a487cc8daa3e1dbc1edb266235c
Nick <nlammers@berkeley.edu>
2025-09-28 09:59:00 -0700

    Prepping for clustrer seg

---
+++ results/20250923/segment_nuclei_20250731.py
@@ -0,0 +1,14 @@
+from src.nucleus_dynamics.build.build01_segment_nuclei_zarr import cellpose_segmentation
+
+
+if __name__ == '__main__':
+
+    # load zarr image file
+    root = "/net/trapnell/vol1/home/nlammers/projects/data/killi_tracker/"
+    model_path = "/net/trapnell/vol1/home/nlammers/projects/data/pecfin_dynamics/built_data/cellpose_training/standard_models/tdTom-bright-log-v5"
+    project_name = "20250731"
+    cellpose_segmentation(root=root,
+                          experiment_date=project_name,
+                          pretrained_model=model_path,
+                          preproc_sigma=[1, 3, 3],
+                          nuclear_channel=0)
