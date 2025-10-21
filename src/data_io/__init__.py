7039cca9a87d9486caeda44593406af41266655d
nlammers371 <nlammers@berkeley.edu>
2025-10-21 15:10:48 -0700

    Add v2 CZI export with multi-side detection

--- src/data_io/__init__.py
+++ src/data_io/__init__.py
@@ -6,6 +6,10 @@ from src.data_io.czi_export import (
     initialize_zarr_store,
     write_zarr,
 )
+from src.data_io.czi_export_v2 import (
+    SideSpec,
+    export_czi_to_zarr_v2,
+)
 from src.data_io.nd2_export import export_nd2_to_zarr, write_to_zarr
 from src.data_io.nd2_metadata import (
     extract_frame_metadata,
@@ -20,6 +24,8 @@ __all__ = [
     "get_prefix_list",
     "initialize_zarr_store",
     "write_zarr",
+    "SideSpec",
+    "export_czi_to_zarr_v2",
     "export_nd2_to_zarr",
     "write_to_zarr",
     "extract_frame_metadata",
